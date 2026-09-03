6b3871436b78ec0210463491f457f03561c6a502
Luannz <luanncc2014@gmail.com>
2026-09-03 09:04:14 -0300

    deu erro

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-^6kmub%5+z!(6m8#3*d9calsw%o*ma@uow+es=8@xz3e58uwx8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
